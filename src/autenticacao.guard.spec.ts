8984bd60d4560fdbc6c6cfc190855e6b4e3ff647
Henrique Melo <hacmelo@gmail.com>
2023-10-24 16:59:17 -0300

    (#136) corrige linhas duplicadas

--- src/autenticacao.guard.spec.ts
+++ src/autenticacao.guard.spec.ts
@@ -12,6 +12,18 @@ describe('AutenticacaoGuard', () => {
     send: jest.fn(),
   };
 
+  const mockContext = {
+    switchToHttp: jest.fn().mockReturnValue({
+      getRequest: jest.fn().mockReturnValue({
+        headers: {
+          authorization: 'Bearer token',
+        },
+      }),
+    }),
+    getHandler: jest.fn(),
+    getClass: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -33,61 +45,28 @@ describe('AutenticacaoGuard', () => {
   });
 
   it('should pass if route is public', async () => {
-    const context = {
-      switchToHttp: jest.fn().mockReturnValue({
-        getRequest: jest.fn().mockReturnValue({
-          headers: {
-            authorization: 'Bearer valid_token',
-          },
-        }),
-      }),
-      getHandler: jest.fn(),
-      getClass: jest.fn(),
-    };
     jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(true);
 
-    const result = await guard.canActivate(context as any);
+    const result = await guard.canActivate(mockContext as any);
 
     expect(result).toBe(true);
   });
 
   it('should pass if authentication is successful', async () => {
-    const context = {
-      switchToHttp: jest.fn().mockReturnValue({
-        getRequest: jest.fn().mockReturnValue({
-          headers: {
-            authorization: 'Bearer valid_token',
-          },
-        }),
-      }),
-      getHandler: jest.fn(),
-      getClass: jest.fn(),
-    };
     jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(false);
     mockClientProxy.send.mockReturnValue(of(true));
 
-    const result = await guard.canActivate(context as any);
+    const result = await guard.canActivate(mockContext as any);
 
     expect(result).toBe(true);
   });
 
   it('should not pass if authentication is unsuccessful', async () => {
-    const context = {
-      switchToHttp: jest.fn().mockReturnValue({
-        getRequest: jest.fn().mockReturnValue({
-          headers: {
-            authorization: 'Bearer invalid_token',
-          },
-        }),
-      }),
-      getHandler: jest.fn(),
-      getClass: jest.fn(),
-    };
     jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(false);
     mockClientProxy.send.mockReturnValue(of(false));
 
     guard
-      .canActivate(context as any)
+      .canActivate(mockContext as any)
       .catch((err) =>
         expect(err).toEqual(
           new UnauthorizedException('Usuário não autenticado!'),
