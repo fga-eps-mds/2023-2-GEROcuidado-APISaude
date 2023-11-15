f7ea27c8e3f117e9a829e97134639dd162bc8841
Pedro Cella <pedrovscella@gmail.com>
2023-11-15 16:18:12 -0300

    (#170) Realiza correcao nos testes unitarios e E2E

--- e2e/idoso.e2e-spec.ts
+++ e2e/idoso.e2e-spec.ts
@@ -29,7 +29,7 @@ describe('E2E - Idoso', () => {
   let client: ClientProxy;
   let repository: Repository<Idoso>;
   let token: string =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5ceyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
   const idoso: Partial<Idoso> = {
     id: undefined,
@@ -40,6 +40,7 @@ describe('E2E - Idoso', () => {
     tipoSanguineo: ETipoSanguineo.AB_Negativo,
     telefoneResponsavel: '123456789',
     descricao: 'desc',
+    dataHora: new Date().toISOString() as any,
   };
 
   beforeAll(async () => {
