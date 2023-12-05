178715fbbfa34cca805d29f2238633cb2a9f4c9d
Thefast777 <221021984@aluno.unb.br>
2023-12-05 14:33:40 -0300

    (#116) cria filter metricas do idoso
    Co-authored-by: Lucas Martins <martin5.luca5.univ@gmail.com>

--- src/metrica/metrica.controller.ts
+++ src/metrica/metrica.controller.ts
@@ -1,50 +1,60 @@
-import { Body, Controller, Get, Param, Patch, Post, Delete } from "@nestjs/common";
-import { MetricaService } from "./metrica.service";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Delete,
+} from '@nestjs/common';
+import { MetricaService } from './metrica.service';
 import { Response } from '../shared/interceptors/data-transform.interceptor';
-import { Paginate, Pagination } from "../shared/decorators/paginate.decorator";
-import { Ordenate, Ordering } from "../shared/decorators/ordenate.decorator";
-import { ResponsePaginate } from "../shared/interfaces/response-paginate.interface";
-import { Metrica } from "./entities/metrica.entity";
-import { IdValidator } from "../shared/validators/id.validator";
-import { UpdateMetricaDto } from "./dto/update-metrica-dto";
-import { HttpResponse } from "../shared/classes/http-response";
-import { CreateMetricaDto } from "./dto/create-metrica-dto";
-import { PublicRoute } from "../shared/decorators/public-route.decorator";
+import { Paginate, Pagination } from '../shared/decorators/paginate.decorator';
+import { Ordenate, Ordering } from '../shared/decorators/ordenate.decorator';
+import { ResponsePaginate } from '../shared/interfaces/response-paginate.interface';
+import { Metrica } from './entities/metrica.entity';
+import { IdValidator } from '../shared/validators/id.validator';
+import { UpdateMetricaDto } from './dto/update-metrica-dto';
+import { HttpResponse } from '../shared/classes/http-response';
+import { CreateMetricaDto } from './dto/create-metrica-dto';
+import { PublicRoute } from '../shared/decorators/public-route.decorator';
+import { Filtering, Filtrate } from '../shared/decorators/filtrate.decorator';
+import { IMetricaFilter } from './interfaces/metrica-filter.interface';
 
 @Controller('metrica')
 export class MetricaController {
-    constructor(private readonly _service: MetricaService) { }
+  constructor(private readonly _service: MetricaService) {}
 
-    @Get()
-    @PublicRoute()
-    async findAll(
-        // @Filtrate() queryParam: Filtering<IRotinaFilter>,
-        @Paginate() pagination: Pagination,
-        @Ordenate() ordering: Ordering,
-    ): Promise<ResponsePaginate<Metrica[]>> {
-        return this._service.findAll(ordering, pagination);
-    }
-    @Get(':id')
-    async findOne(@Param() param: IdValidator): Promise<Metrica> {
-        return this._service.findOne(param.id);
-    }
-    @Patch(':id')
-    async update(
-        @Param() param: IdValidator,
-        @Body() body: UpdateMetricaDto,
-    ): Promise<Response<Metrica>> {
-        const updated = await this._service.update(param.id, body);
-        return new HttpResponse<Metrica>(updated).onUpdated();
-    }
-    @Post()
-    async create(@Body() body: CreateMetricaDto) {
-        const created = await this._service.create(body);
-        return new HttpResponse<Metrica>(created).onCreated();
-    }
+  @Get()
+  @PublicRoute()
+  async findAll(
+    @Filtrate() queryParam: Filtering<IMetricaFilter>,
+    @Paginate() pagination: Pagination,
+    @Ordenate() ordering: Ordering,
+  ): Promise<ResponsePaginate<Metrica[]>> {
+    return this._service.findAll(queryParam.filter, ordering, pagination);
+  }
+  @Get(':id')
+  async findOne(@Param() param: IdValidator): Promise<Metrica> {
+    return this._service.findOne(param.id);
+  }
+  @Patch(':id')
+  async update(
+    @Param() param: IdValidator,
+    @Body() body: UpdateMetricaDto,
+  ): Promise<Response<Metrica>> {
+    const updated = await this._service.update(param.id, body);
+    return new HttpResponse<Metrica>(updated).onUpdated();
+  }
+  @Post()
+  async create(@Body() body: CreateMetricaDto) {
+    const created = await this._service.create(body);
+    return new HttpResponse<Metrica>(created).onCreated();
+  }
 
-    @Delete(':id')
-    async remove(@Param() param: IdValidator): Promise<Response<unknown>> {
-        const deleted = await this._service.remove(param.id);
-        return new HttpResponse(deleted).onDeleted();
-    }
+  @Delete(':id')
+  async remove(@Param() param: IdValidator): Promise<Response<unknown>> {
+    const deleted = await this._service.remove(param.id);
+    return new HttpResponse(deleted).onDeleted();
+  }
 }
--- src/metrica/metrica.service.ts
+++ src/metrica/metrica.service.ts
@@ -3,93 +3,75 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Ordering } from '../shared/decorators/ordenate.decorator';
 import { Pagination } from '../shared/decorators/paginate.decorator';
-// import { getWhereClauseNumber } from '../shared/helpers/sql-query-helper';
+import { getWhereClauseNumber } from '../shared/helpers/sql-query-helper';
 import { ResponsePaginate } from '../shared/interfaces/response-paginate.interface';
 import { CreateMetricaDto } from './dto/create-metrica-dto';
 import { Metrica } from './entities/metrica.entity';
 import { UpdateMetricaDto } from './dto/update-metrica-dto';
+import { IMetricaFilter } from './interfaces/metrica-filter.interface';
 
 @Injectable()
 export class MetricaService {
-    constructor(
-        @InjectRepository(Metrica)
-        private readonly _repository: Repository<Metrica>,
-    ) { }
+  constructor(
+    @InjectRepository(Metrica)
+    private readonly _repository: Repository<Metrica>,
+  ) {}
 
-    async create(body: CreateMetricaDto): Promise<Metrica> {
-        const metrica = new Metrica(body);
-        return this._repository.save(metrica);
-    }
+  async create(body: CreateMetricaDto): Promise<Metrica> {
+    const metrica = new Metrica(body);
+    return this._repository.save(metrica);
+  }
 
-    async findOne(id: number) {
-        const metrica = await this._repository.findOneOrFail({ where: { id } });
-        return metrica;
-    }
+  async findOne(id: number) {
+    const metrica = await this._repository.findOneOrFail({ where: { id } });
+    return metrica;
+  }
 
-    async update(id: number, body: UpdateMetricaDto): Promise<Metrica> {
-        const found = await this.findOne(id);
-        const merged = Object.assign(found, body);
+  async update(id: number, body: UpdateMetricaDto): Promise<Metrica> {
+    const found = await this.findOne(id);
+    const merged = Object.assign(found, body);
 
-        const updated = await this._repository.save(merged);
+    const updated = await this._repository.save(merged);
 
-        return updated;
-    }
+    return updated;
+  }
 
-    async findAll(
-        // filter: IRotinaFilter,
-        ordering: Ordering,
-        paging: Pagination,
-    ): Promise<ResponsePaginate<Metrica[]>> {
-        const limit = paging.limit;
-        const offset = paging.offset;
-        const sort = ordering.column;
-        const order = ordering.dir.toUpperCase() as 'ASC' | 'DESC';
-        // const where = this.buildWhereClause(filter);
+  async findAll(
+    filter: IMetricaFilter,
+    ordering: Ordering,
+    paging: Pagination,
+  ): Promise<ResponsePaginate<Metrica[]>> {
+    const limit = paging.limit;
+    const offset = paging.offset;
+    const sort = ordering.column;
+    const order = ordering.dir.toUpperCase() as 'ASC' | 'DESC';
+    const where = this.buildWhereClause(filter);
 
-        const [result, total] = await this._repository
-            .createQueryBuilder('metrica')
-            // .where(`${where}`)
-            .limit(limit)
-            .offset(offset)
-            .orderBy(`"${sort}"`, order)
-            .getManyAndCount();
+    const [result, total] = await this._repository
+      .createQueryBuilder('metrica')
+      .where(`${where}`)
+      .limit(limit)
+      .offset(offset)
+      .orderBy(`"${sort}"`, order)
+      .getManyAndCount();
 
-        return {
-            data: result,
-            count: +total,
-            pageSize: +total,
-        };
-    }
+    return {
+      data: result,
+      count: +total,
+      pageSize: +total,
+    };
+  }
 
-    // private buildWhereClause(filter: IRotinaFilter): string {
-    //     let whereClause = '1 = 1 ';
+  private buildWhereClause(filter: IMetricaFilter): string {
+    let whereClause = '1 = 1 ';
 
-    //     whereClause += getWhereClauseNumber(filter.id, 'id');
-    //     whereClause += getWhereClauseNumber(filter.idIdoso, '"idIdoso"');
-    //     whereClause += this.getWhereClauseDate(filter.dataHora);
+    whereClause += getWhereClauseNumber(filter.idIdoso, '"idIdoso"');
 
-    //     return whereClause;
-    // }
+    return whereClause;
+  }
 
-    // private getWhereClauseDate(value: string | undefined): string {
-    //     if (!value || value.length < 1) return '';
-
-    //     const date = new Date(value);
-    //     const weekday = date.getDay();
-
-    //     const start = new Date(value);
-    //     start.setUTCHours(0, 0, 0);
-    //     const startString = start.toISOString();
-
-    //     const end = new Date(value);
-    //     end.setUTCHours(23, 59, 59);
-    //     const endString = end.toISOString();
-
-    //     return ` AND (("dataHora"::date BETWEEN '${startString}'::date AND '${endString}'::date) OR ("dias" && '{${weekday}}'::rotina_dias_enum[]))`;
-    // }
-
-    async remove(id: number) {
-        const found = await this._repository.findOneOrFail({ where: { id } });
-        return this._repository.remove(found);
-    }
+  async remove(id: number) {
+    const found = await this._repository.findOneOrFail({ where: { id } });
+    return this._repository.remove(found);
+  }
 }
