688588a014d918e2006fcd66467c0b89fc83319b
martinsglucas <martin5.luca5.univ@gmail.com>
2023-12-07 14:04:09 -0300

    (#116) adiciona filtro da ValorMetrica

---
+++ src/valorMetrica/interfaces/valorMetrica-filter.interface.ts
@@ -0,0 +1,3 @@
+export interface IValorMetricaFilter {
+    idMetrica?: number;
+}
--- src/valorMetrica/valorMetrica.service.ts
+++ src/valorMetrica/valorMetrica.service.ts
@@ -3,83 +3,65 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Ordering } from '../shared/decorators/ordenate.decorator';
 import { Pagination } from '../shared/decorators/paginate.decorator';
-// import { getWhereClauseNumber } from '../shared/helpers/sql-query-helper';
+import { getWhereClauseNumber } from '../shared/helpers/sql-query-helper';
 import { ResponsePaginate } from '../shared/interfaces/response-paginate.interface';
 import { CreateValorMetricaDto } from './dto/create-valorMetrica-dto';
 import { ValorMetrica } from './entities/valorMetrica.entity';
+import { IValorMetricaFilter } from './interfaces/valorMetrica-filter.interface';
 
 @Injectable()
 export class ValorMetricaService {
-    constructor(
-        @InjectRepository(ValorMetrica)
-        private readonly _repository: Repository<ValorMetrica>,
-    ) { }
+  constructor(
+    @InjectRepository(ValorMetrica)
+    private readonly _repository: Repository<ValorMetrica>,
+  ) { }
 
-    async create(body: CreateValorMetricaDto): Promise<ValorMetrica> {
-        const valorMetrica = new ValorMetrica(body);
-        return this._repository.save(valorMetrica);
-    }
+  async create(body: CreateValorMetricaDto): Promise<ValorMetrica> {
+    const valorMetrica = new ValorMetrica(body);
+    return this._repository.save(valorMetrica);
+  }
 
-    async findOne(id: number) {
-        const metrica = await this._repository.findOneOrFail({ where: { id } });
-        return metrica;
-    }
+  async findOne(id: number) {
+    const metrica = await this._repository.findOneOrFail({ where: { id } });
+    return metrica;
+  }
 
-    async findAll(
-        // filter: IRotinaFilter,
-        ordering: Ordering,
-        paging: Pagination,
-    ): Promise<ResponsePaginate<ValorMetrica[]>> {
-        const limit = paging.limit;
-        const offset = paging.offset;
-        const sort = ordering.column;
-        const order = ordering.dir.toUpperCase() as 'ASC' | 'DESC';
-        // const where = this.buildWhereClause(filter);
+  async findAll(
+    filter: IValorMetricaFilter,
+    ordering: Ordering,
+    paging: Pagination,
+  ): Promise<ResponsePaginate<ValorMetrica[]>> {
+    const limit = paging.limit;
+    const offset = paging.offset;
+    const sort = ordering.column;
+    const order = ordering.dir.toUpperCase() as 'ASC' | 'DESC';
+    const where = this.buildWhereClause(filter);
 
-        const [result, total] = await this._repository
-            .createQueryBuilder('valorMetrica')
-            // .where(`${where}`)
-            .limit(limit)
-            .offset(offset)
-            .orderBy(`"${sort}"`, order)
-            .getManyAndCount();
+    const [result, total] = await this._repository
+      .createQueryBuilder('valorMetrica')
+      .where(`${where}`)
+      .limit(limit)
+      .offset(offset)
+      .orderBy(`"${sort}"`, order)
+      .getManyAndCount();
 
-        return {
-            data: result,
-            count: +total,
-            pageSize: +total,
-        };
-    }
+    return {
+      data: result,
+      count: +total,
+      pageSize: +total,
+    };
+  }
 
-    // private buildWhereClause(filter: IRotinaFilter): string {
-    //     let whereClause = '1 = 1 ';
+  private buildWhereClause(filter: IValorMetricaFilter): string {
+    let whereClause = '1 = 1 ';
 
-    //     whereClause += getWhereClauseNumber(filter.id, 'id');
-    //     whereClause += getWhereClauseNumber(filter.idIdoso, '"idIdoso"');
-    //     whereClause += this.getWhereClauseDate(filter.dataHora);
+    whereClause += getWhereClauseNumber(filter.idMetrica, 'idMetrica');
 
-    //     return whereClause;
-    // }
+    return whereClause;
+  }
 
-    // private getWhereClauseDate(value: string | undefined): string {
-    //     if (!value || value.length < 1) return '';
-
-    //     const date = new Date(value);
-    //     const weekday = date.getDay();
-
-    //     const start = new Date(value);
-    //     start.setUTCHours(0, 0, 0);
-    //     const startString = start.toISOString();
-
-    //     const end = new Date(value);
-    //     end.setUTCHours(23, 59, 59);
-    //     const endString = end.toISOString();
-
-    //     return ` AND (("dataHora"::date BETWEEN '${startString}'::date AND '${endString}'::date) OR ("dias" && '{${weekday}}'::rotina_dias_enum[]))`;
-    // }
-
-    async remove(id: number) {
-        const found = await this._repository.findOneOrFail({ where: { id } });
-        return this._repository.remove(found);
-    }
+  async remove(id: number) {
+    const found = await this._repository.findOneOrFail({ where: { id } });
+    return this._repository.remove(found);
+  }
 }
